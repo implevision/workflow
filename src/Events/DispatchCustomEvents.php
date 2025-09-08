687cf177412d3555ffd1319ffb61ec32c25f35d2
Jimish Gamit <unique.jimish@gmail.com>
2025-09-08 19:57:46 +0530

    fix: custom event

--- src/Events/DispatchCustomEvents.php
+++ src/Events/DispatchCustomEvents.php
@@ -4,52 +4,15 @@ namespace Taurus\Workflow\Events;
 
 trait DispatchCustomEvents
 {
-    /**
-     * User exposed observable events.
-     *
-     * These are extra user-defined events observers may subscribe to.
-     *
-     * @var array
-     */
-    protected $observables = [];
 
-    /**
-     * Get the observable event names.
-     *
-     * @return array
-     */
-    public function getObservableEvents()
-    {
-        return array_merge(
-            [
-                'created',
-                'updated',
-                'deleted',
-            ],
-            $this->observables
-        );
-    }
-
-    /**
-     * Set the observable event names.
-     *
-     * @return $this
-     */
-    public function setObservableEvents(array $observables)
-    {
-        $this->observables = $observables;
-
-        return $this;
-    }
 
     protected function fireEvent($event, $eventVirtualModel, $data = [], $recordIdentifier = null, $halt = false)
     {
         $data = array_merge($data, ['recordIdentifier' => $recordIdentifier]);
-        if (in_array($event, $this->getObservableEvents())) {
-            ! $halt ?
-                event(new AsyncCustomEvents($event, $eventVirtualModel, $data)) :
-                event(new CustomEvents($event, $eventVirtualModel, $data));
-        }
+        ! $halt ?
+            event(new AsyncCustomEvents($event, $eventVirtualModel, $data)) :
+            event(new CustomEvents($event, $eventVirtualModel, $data));
+
 
         return true;
     }
