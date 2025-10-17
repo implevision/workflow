1d1de534f7aab8ff66dac54f0ef3c2e76aa45fd0
pratikheda1 <pratik.heda@thinktaurus.com>
2025-10-17 16:49:43 +0530

    improved validation

--- src/Http/Rules/ValidCustomDateTimeInfo.php
+++ src/Http/Rules/ValidCustomDateTimeInfo.php
@@ -16,43 +16,49 @@ class ValidCustomDateTimeInfo implements ValidationRule
     {
         $effectiveAction = request()->input('when.effectiveActionToExecuteWorkflow');
 
-        $isValid = true;
-
         if ($effectiveAction === 'CUSTOM_DATE_AND_TIME') {
-            // Validate cron fields
             $cronFields = [
-                'cronDayOfMonth',
-                'cronDayOfWeek',
-                'cronHours',
-                'cronMinutes',
-                'cronMonth',
-                'cronYear',
+                'cronMinutes' => [
+                    'label' => 'Minutes',
+                    'regex' => '/^([*]|[0-5]?\d)(-([0-5]?\d))?(\/\d+)?(,([*]|[0-5]?\d)(-([0-5]?\d))?(\/\d+)?)*$/',
+                    'error' => 'Minutes must be a valid cron expression (0-59, *, ranges, lists, steps).',
+                ],
+                'cronHours' => [
+                    'label' => 'Hours',
+                    'regex' => '/^([*]|[01]?\d|2[0-3])(-([01]?\d|2[0-3]))?(\/\d+)?(,([*]|[01]?\d|2[0-3])(-([01]?\d|2[0-3]))?(\/\d+)?)*$/',
+                    'error' => 'Hours must be a valid cron expression (0-23, *, ranges, lists, steps).',
+                ],
+                'cronDayOfMonth' => [
+                    'label' => 'Day of Month',
+                    'regex' => '/^([*?]|[1-9]|[12]\d|3[01])(-([1-9]|[12]\d|3[01]))?(\/\d+)?(,([*?]|[1-9]|[12]\d|3[01])(-([1-9]|[12]\d|3[01]))?(\/\d+)?)*$/',
+                    'error' => 'Day of Month must be a valid cron expression (1-31, *, ?, ranges, lists, steps).',
+                ],
+                'cronMonth' => [
+                    'label' => 'Month',
+                    'regex' => '/^([*]|[1-9]|1[0-2])(-([1-9]|1[0-2]))?(\/\d+)?(,([*]|[1-9]|1[0-2])(-([1-9]|1[0-2]))?(\/\d+)?)*$/',
+                    'error' => 'Month must be a valid cron expression (1-12, *, ranges, lists, steps).',
+                ],
+                'cronDayOfWeek' => [
+                    'label' => 'Day of Week',
+                    'regex' => '/^([*?]|[0-6])(-[0-6])?(\/\d+)?(,([*?]|[0-6])(-[0-6])?(\/\d+)?)*$/',
+                    'error' => 'Day of Week must be a valid cron expression (0-6, *, ?, ranges, lists, steps).',
+                ],
+                'cronYear' => [
+                    'label' => 'Year',
+                    'regex' => '/^([*]|\d{4})(-\d{4})?(\/\d+)?(,([*]|\d{4})(-\d{4})?(\/\d+)?)*$/',
+                    'error' => 'Year must be a valid cron expression (4-digit, *, ranges, lists, steps).',
+                ],
             ];
 
-            foreach ($cronFields as $cronField) {
-                if (! isset($value[$cronField])) {
-                    $isValid = false;
-                    $fail("$cronField is required.");
-
+            foreach ($cronFields as $field => $meta) {
+                if (!isset($value[$field])) {
+                    $fail("Value of {$meta['label']} is required.");
                     continue;
                 }
-                // Accept only numbers or * for all except hours and minutes
-                if (in_array($cronField, ['cronHours', 'cronMinutes'])) {
-                    if (! preg_match('/^(\*|\d{1,2})$/', (string) $value[$cronField])) {
-                        $isValid = false;
-                        $fail("$cronField must be a number (0-59 for minutes, 0-23 for hours) or '*'.");
-                    }
-                } else {
-                    if (! preg_match('/^(\*|\d{1,2})$/', (string) $value[$cronField])) {
-                        $isValid = false;
-                        $fail("$cronField must be a number (1-31 for day of month, 1-12 for month, 0-6 for day of week, or year) or '*'.");
-                    }
+                if (!preg_match($meta['regex'], (string)$value[$field])) {
+                    $fail($meta['error']);
                 }
             }
-        }
-
-        if (! $isValid) {
-            $fail('There are errors in the custom date and time information.');
         }
     }
 }
