ec1f70a2109680e05bbbb32318d63a49a8c1b4fa
Jimish Gamit <unique.jimish@gmail.com>
2025-11-28 17:05:39 +0530

    [Feature] Webhook request implementation (ODS-9965) (#25)

--- src/Services/Auth/AbstractAuthService.php
+++ src/Services/Auth/AbstractAuthService.php
@@ -2,9 +2,6 @@
 
 namespace Taurus\Workflow\Services\Auth;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
-
 /**
  * Class AbstractAuthService
  *
@@ -28,45 +25,5 @@ class AbstractAuthService
     {
         // This method should be implemented by subclasses
         throw new \BadMethodCallException('Method authenticate() must be implemented in subclass.');
-    }
-
-    /**
-     * Makes an HTTP request.
-     *
-     * @param  string  $method  The HTTP method to use (e.g., GET, POST, PUT, DELETE).
-     * @param  string  $url  The URL to which the request is sent.
-     * @param  array  $headers  Optional. An associative array of headers to send with the request.
-     * @param  array  $body  Optional. An array representing the body of the request.
-     * @return mixed The response from the request, which may vary based on the implementation.
-     */
-    public function makeRequest($method, $url, $headers = [], $body = [])
-    {
-        $client = new Client;
-
-        if (empty($headers['Content-Type'])) {
-            $headers['Content-Type'] = 'application/x-www-form-urlencoded';
-        }
-
-        $options = ['headers' => $headers];
-
-        $contentType = strtolower($headers['Content-Type']);
-        switch ($contentType) {
-            case 'application/json':
-                $options['json'] = json_encode($body);
-                break;
-            case 'application/x-www-form-urlencoded':
-                $options['form_params'] = $body;
-                break;
-            default:
-                throw new \InvalidArgumentException('Unsupported Content-Type: '.$contentType);
-        }
-
-        try {
-            $response = $client->request($method, $url, $options);
-
-            return json_decode($response->getBody(), true);
-        } catch (RequestException $e) {
-            throw new \Exception('HTTP Request failed: '.$e->getMessage());
-        }
     }
 }
