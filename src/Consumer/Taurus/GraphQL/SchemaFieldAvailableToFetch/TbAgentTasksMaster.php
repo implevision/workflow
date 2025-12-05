74cf852e8474afaec9508fd452388ba65e8dd5e8
Jimish Gamit <unique.jimish@gmail.com>
2025-12-05 16:49:36 +0530

    fix: agent code length

--- src/Consumer/Taurus/GraphQL/SchemaFieldAvailableToFetch/TbAgentTasksMaster.php
+++ src/Consumer/Taurus/GraphQL/SchemaFieldAvailableToFetch/TbAgentTasksMaster.php
@@ -343,6 +343,7 @@ class TbAgentTasksMaster
                     ],
                 ],
                 'jqFilter' => '.agentTask.policyTransaction.TbPersoninfo.additionalInfo.wyoAgencyAgentCode',
+                'parseResultCallback' => 'parseWyoAgencyAgentCode',
             ],
             'UUID' => [
                 'GraphQLschemaToReplace' => [],
@@ -446,15 +447,15 @@ class TbAgentTasksMaster
         // TODO: TMP fix. Need to covert to actual one
         $coverageData = \DB::select(
             'SELECT cvgt.n_CvgSegmentGrossPremium AS coverage_premium
-from tb_potransactions pot
-left join tb_policies pol on pol.n_PolicyNoId_PK = pot.n_PolicyMaster_FK
-left join tb_pocoveragetrans cvgt on cvgt.n_POTransactionFK = pot.n_potransaction_PK
-left join tb_pocoverageschedules cvgs on cvgs.n_POCoverageSchedule_PK = cvgt.n_POCoverageScheduleFK
-left join tb_pocoveragemasters cvgm on cvgm.n_POCoverageMaster_PK = cvgs.n_POCoverageMasterFk
-left join tb_cvgpccoverages cvgp on cvgp.n_PCCoverageID_PK = cvgm.n_PRCoverageFK
-left join tb_poriskmasters risk on risk.n_PORiskMaster_PK = cvgs.n_PORiskMasterFK
-where pot.n_potransaction_PK IN(:transactionId)
-AND cvgp.s_CoverageCode = :coverageCode',
+                from tb_potransactions pot
+                left join tb_policies pol on pol.n_PolicyNoId_PK = pot.n_PolicyMaster_FK
+                left join tb_pocoveragetrans cvgt on cvgt.n_POTransactionFK = pot.n_potransaction_PK
+                left join tb_pocoverageschedules cvgs on cvgs.n_POCoverageSchedule_PK = cvgt.n_POCoverageScheduleFK
+                left join tb_pocoveragemasters cvgm on cvgm.n_POCoverageMaster_PK = cvgs.n_POCoverageMasterFk
+                left join tb_cvgpccoverages cvgp on cvgp.n_PCCoverageID_PK = cvgm.n_PRCoverageFK
+                left join tb_poriskmasters risk on risk.n_PORiskMaster_PK = cvgs.n_PORiskMasterFK
+                where pot.n_potransaction_PK IN(:transactionId)
+                AND cvgp.s_CoverageCode = :coverageCode',
             ['coverageCode' => $coverageCode, 'transactionId' => $transactionId]
         );
 
@@ -487,5 +488,10 @@ AND cvgp.s_CoverageCode = :coverageCode',
         }
 
         return $dueDateArr;
+    }
+
+    public function parseWyoAgencyAgentCode($agentCode)
+    {
+        return (strlen($agentCode) === 7) ? substr_replace($agentCode, '', 4, 1) : $agentCode;
     }
 }
