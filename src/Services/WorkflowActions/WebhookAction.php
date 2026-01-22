6f56921b7e0fb8d02b8d236f724daa7e694defa8
Jimish Gamit <unique.jimish@gmail.com>
2026-01-22 19:18:49 +0530

    fix: webhook placeholder issue

--- src/Services/WorkflowActions/WebhookAction.php
+++ src/Services/WorkflowActions/WebhookAction.php
@@ -148,8 +148,8 @@ class WebhookAction extends AbstractWorkflowAction
         if ($data) {
             preg_match_all('/{{\s*(.*?)\s*}}/', $webhookRequestUrl, $webhookRequestUrlPlaceholderMatches);
             foreach ($data as $placeHolderData) {
-                $requestUrl = $this->replacePlaceholders($webhookRequestUrl, $placeHolderData);
-                $requestPayload = $this->replacePlaceholders($webhookRequestPayload, $placeHolderData);
+                $requestUrl = $this->replacePlaceholders($webhookRequestUrl, $placeHolderData, true);
+                $requestPayload = $this->replacePlaceholders($webhookRequestPayload, $placeHolderData, true);
                 try {
                     Http::makeRequest($webhookRequestMethod, $requestUrl, $webhookRequestHeaders, $requestPayload);
                 } catch (\Exception $e) {
@@ -159,18 +159,20 @@ class WebhookAction extends AbstractWorkflowAction
         }
     }
 
-    private function replacePlaceholders($input, $placeholders)
+    private function replacePlaceholders($input, $placeholders, $replaceWithEmptySpaceIfNotAvailable = false)
     {
         if (is_array($input)) {
-            return array_map(function ($item) use ($placeholders) {
-                return $this->replacePlaceholders($item, $placeholders);
+            return array_map(function ($item) use ($placeholders, $replaceWithEmptySpaceIfNotAvailable) {
+                return $this->replacePlaceholders($item, $placeholders, $replaceWithEmptySpaceIfNotAvailable);
             }, $input);
         }
 
-        return preg_replace_callback('/{{\s*(.*?)\s*}}/', function ($matches) use ($placeholders) {
+        return preg_replace_callback('/{{\s*(.*?)\s*}}/', function ($matches) use ($placeholders, $replaceWithEmptySpaceIfNotAvailable) {
             $placeholder = $matches[1];
 
-            return array_key_exists($placeholder, $placeholders) ? $placeholders[$placeholder] : '{{'.$placeholder.'}}';
+            $defaultValue = $replaceWithEmptySpaceIfNotAvailable ? '' : '{{'.$placeholder.'}}';
+
+            return array_key_exists($placeholder, $placeholders) ? $placeholders[$placeholder] : $defaultValue;
         }, $input);
     }
 
