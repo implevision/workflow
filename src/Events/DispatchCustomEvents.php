bde6185461115e87916c00275e0a64c384651770
Jimish Gamit <unique.jimish@gmail.com>
2026-02-04 14:50:41 +0530

    fix: on demand workflow

--- src/Events/DispatchCustomEvents.php
+++ src/Events/DispatchCustomEvents.php
@@ -4,12 +4,12 @@ namespace Taurus\Workflow\Events;
 
 trait DispatchCustomEvents
 {
-    public static function fireEvent($event, $eventVirtualModel, $data = [], $recordIdentifier = null, $halt = false)
+    public static function fireEvent($event, $eventVirtualModel, $data = [], $recordIdentifier = null, $appendPlaceHolders = [], $halt = false)
     {
         $data = array_merge($data, ['recordIdentifier' => $recordIdentifier]);
         ! $halt ?
-            event(new AsyncCustomEvents($event, $eventVirtualModel, $data)) :
-            event(new CustomEvents($event, $eventVirtualModel, $data));
+            event(new AsyncCustomEvents($event, $eventVirtualModel, $data, $appendPlaceHolders)) :
+            event(new CustomEvents($event, $eventVirtualModel, $data, $appendPlaceHolders));
 
         return true;
     }
