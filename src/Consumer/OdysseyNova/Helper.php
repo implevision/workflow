880d46d0e8d2e7701e65647d0bbbbfdc86f83a08
Jimish Gamit <unique.jimish@gmail.com>
2026-03-02 15:28:48 +0530

    fix: Odyssey Nova setup

---
+++ src/Consumer/OdysseyNova/Helper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Taurus\Workflow\Consumer\OdysseyNova;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Storage;
+
+class Helper {}
---
+++ src/Consumer/OdysseyNova/PostAction/PostActionService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Taurus\Workflow\Consumer\Taurus\PostAction;
+
+class PostActionService
+{
+    public function execute() {}
+}
