de5ce7fcc2ad1f4bf43b89e5471588afe9997da2
pratikheda1 <pratik.heda@thinktaurus.com>
2026-05-26 15:09:50 +0530

    add post action in workflow output

---
+++ src/Services/WorkflowActions/Helpers/WorkflowOutput/PrepareWorkflowOutputData.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Taurus\Workflow\Services\WorkflowActions\Helpers\WorkflowOutput;
+
+use Taurus\Workflow\Events\PostActionEvent;
+
+class PrepareWorkflowOutputData
+{
+    protected $workflowId;
+
+    protected $jobWorkflowId;
+
+    protected $recordIdentifier;
+
+    protected $templateId;
+
+    protected $payload;
+
+    protected $templateInformation;
+
+    public function prepare($workflowId, $jobWorkflowId, $recordIdentifier, $templateId, $payload, $templateInformation)
+    {
+        $this->workflowId = $workflowId;
+        $this->jobWorkflowId = $jobWorkflowId;
+        $this->recordIdentifier = $recordIdentifier;
+        $this->templateId = $templateId;
+        $this->payload = $payload;
+        $this->templateInformation = $templateInformation;
+
+        return $this;
+    }
+
+    public function execute()
+    {
+        $data = $this->payload['data'] ?? [];
+        $postAction = $this->payload['postAction'] ?? '';
+        $outputActionType = $this->payload['actionPayload']['outputActionType'] ?? '';
+
+        $this->generateOutput($outputActionType, $data);
+
+        if ($postAction) {
+            $actionPayload = [
+                'workflowId' => $this->workflowId,
+                'jobWorkflowId' => $this->jobWorkflowId,
+                'recordIdentifier' => $this->recordIdentifier,
+                'actionType' => 'workflow_output',
+                'data' => $data,
+                'payload' => $data,
+                'emailTemplate' => $this->templateInformation['html'] ?? '',
+                'subject' => $this->templateInformation['subject'] ?? '',
+                'outputActionType' => $outputActionType,
+                'postAction' => $postAction,
+                'actionPayload' => $this->payload['actionPayload'] ?? [],
+                'letterEditorMode' => $this->templateInformation['letterEditorMode'] ?? null,
+                'pdfS3Key' => $this->templateInformation['pdfS3Key'] ?? null,
+                'pdfPlaceholders' => $this->templateInformation['pdfPlaceholders'] ?? null,
+                'module' => getModuleForCurrentWorkflow(),
+            ];
+
+            \Log::info('WORKFLOW - Executing post action for workflow output');
+            event(new PostActionEvent($actionPayload['module'], $actionPayload, (string) $this->jobWorkflowId));
+        }
+    }
+
+    private function generateOutput(string $outputActionType, array $data)
+    {
+        switch ($outputActionType) {
+            case 'PRINT_AS_PDF':
+                \Log::info('WORKFLOW - Generating PDF output');
+                $printAsPdf = new PrintAsPdf;
+                $printAsPdf->generate($this->jobWorkflowId, $data, $this->templateInformation);
+                break;
+
+            default:
+                throw new \Exception("Unsupported output action type: {$outputActionType}");
+        }
+    }
+}
--- src/Services/WorkflowActions/WorkflowOutputAction.php
+++ src/Services/WorkflowActions/WorkflowOutputAction.php
@@ -2,7 +2,7 @@
 
 namespace Taurus\Workflow\Services\WorkflowActions;
 
-use Taurus\Workflow\Services\WorkflowActions\Helpers\WorkflowOutput\PrintAsPdf;
+use Taurus\Workflow\Services\WorkflowActions\Helpers\WorkflowOutput\PrepareWorkflowOutputData;
 use Taurus\Workflow\Services\WorkflowEmailService;
 
 class WorkflowOutputAction extends AbstractWorkflowAction
@@ -44,20 +44,22 @@ class WorkflowOutputAction extends AbstractWorkflowAction
 
     public function execute()
     {
+        $workflowId = $this->getWorkflowId();
         $jobWorkflowId = $this->getJobWorkflowId();
+        $recordIdentifier = $this->getRecordIdentifier();
         $data = $this->getData();
         $payload = $this->getPayload();
 
-        $outputActionType = $payload['outputActionType'] ?? '';
-
-        switch ($outputActionType) {
-            case 'PRINT_AS_PDF':
-                \Log::info('WORKFLOW - Generating PDF output');
-                $printAsPdf = new PrintAsPdf;
-                $printAsPdf->generate($jobWorkflowId, $data, $this->templateInformation);
-                break;
-            default:
-                throw new \Exception("Unsupported output action type: {$outputActionType}");
+        try {
+            \Log::info('WORKFLOW - Preparing workflow output data');
+            $prepareWorkflowOutputData = new PrepareWorkflowOutputData;
+            $prepareWorkflowOutputData->prepare($workflowId, $jobWorkflowId, $recordIdentifier, $payload['id'], [
+                'data' => $data,
+                'postAction' => ! empty($payload['postAction']) ? $payload['postAction'] : '',
+                'actionPayload' => $payload ?? [],
+            ], $this->templateInformation)->execute();
+        } catch (\Exception $e) {
+            throw $e;
         }
     }
 }
