04eba1fa88553fdbbc9e6633bf7bd54eaac9e095
Sourabhthinktaurus <sourabh.dhanariya@thinktaurus.com>
2026-06-25 13:22:21 +0530

    fix: remove trailing whitespace

--- src/Consumer/Taurus/GraphQL/SchemaFieldAvailableToFetch/TbPotransaction.php
+++ src/Consumer/Taurus/GraphQL/SchemaFieldAvailableToFetch/TbPotransaction.php
@@ -1098,7 +1098,7 @@ class TbPotransaction extends AbstractSchema
                 ],
             ],
         ];
-        
+
         $taskListJqFilter = $targetTaskId !== null
             ? "[.policyQuery.agentTasks[]?.taskMapping[]? | select((.id|tostring) == ({$targetTaskId}|tostring))]"
             : '[.policyQuery.agentTasks[]?.taskMapping[]?]';
