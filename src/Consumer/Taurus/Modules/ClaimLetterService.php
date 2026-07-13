63de388ffd56f4ea2e487ec80507e996156ad0b0
pratikheda1 <162109953+pratikheda1@users.noreply.github.com>
2026-07-13 15:28:30 +0530

    [Feature] Add claim letter service (ODS-11796) (#180)

---
+++ src/Consumer/Taurus/Modules/ClaimLetterService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Taurus\Workflow\Consumer\Taurus\Modules;
+
+use App\Services\Claim\OnDemandWorkflowService;
+
+class ClaimLetterService extends ModuleService
+{
+    public function getPostFixForTaskDefinition()
+    {
+        return 'claim';
+    }
+
+    public function getExtendedTemplateInfo(array $templatePayload = []): array
+    {
+        try {
+            $payload = [
+                'recordIdentifier' => getRecordIdentifierForRunningWorkflow() ?? null,
+                'workflowTemplateId' => $templatePayload['id'] ?? null,
+            ];
+
+            $response = OnDemandWorkflowService::fetchExtendedTemplateInfo($payload);
+
+            return $response;
+        } catch (\Exception $e) {
+            \Log::error('WORKFLOW - Error getting extended template info for TbClaim.', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+                'line' => $e->getLine(),
+                'file' => $e->getFile(),
+            ]);
+
+            return [];
+        }
+    }
+}
